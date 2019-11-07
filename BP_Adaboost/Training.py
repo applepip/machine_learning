57f4cfa78ebdcf2082404016e3fc82a2699c3f3b
leon <applepip@163.com>
2019-11-07 17:08:19 +0800

    delete adaboost

--- BP_Adaboost/Training.py
+++
@@ -1,8 +0,0 @@
-from Data import *
-from BPnetwork import *
-
-print(len(input_train[0]))
-
-model = BPnetwork(input_train, output_train)
-result = model.evaluate(input_test, output_test)
-print("Test accuracy:", result[1])
