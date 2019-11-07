3faa6cd277654ef838050ae24358c9ff97672231
leon <applepip@163.com>
2019-11-07 17:41:00 +0800

    data analysing

---
+++ money_happy/data_plot.py
@@ -0,0 +1,10 @@
+import matplotlib as plt
+import pandas as pd
+
+gdp_per_capita = pd.read_csv("lifesat/gdp_per_capita.csv", thousands=',', delimiter='\t',
+                             encoding='latin1', na_values="n/a", index_col="Country")
+gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
+
+
+
+print(gdp_per_capita["GDP per capita"])
