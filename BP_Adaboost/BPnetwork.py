b7735cccdc1964cd451b32803d7dfe8baf5f74bf
leon <applepip@163.com>
2019-11-07 14:57:28 +0800

    create bpnetwork

--- BP_Adaboost/BPnetwork.py
+++ BP_Adaboost/BPnetwork.py
@@ -1,19 +1,21 @@
 import tensorflow as tf
 
-def BPnetwork(x_train, y_train, modelfile):
+def BPnetwork(x_train, y_train):
     #使用层次模型
     model = tf.keras.Sequential()
-    #输入层与第一个隐层同时建立，输入层13维，第一个隐层有10个神经元
-    model.add(tf.keras.Dense(10, input_dim=13))
+    #输入层与第一个隐层同时建立，输入层10维，第一个隐层有10个神经元
+    model.add(tf.keras.layers.Dense(10, input_dim=10))
     # 添加激活函数
-    model.add(tf.keras.Activation('relu'))
+    model.add(tf.keras.layers.Activation('relu'))
     #输出层
-    model.add(tf.keras.Dense(1,input_dim=12))
+    model.add(tf.keras.layers.Dense(1, input_dim=10))
+    # 添加激活函数
+    model.add(tf.keras.layers.Activation('sigmoid'))
     #编译模型
-    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'acc'])
     #训练模型1000次
-    model.fit(x_train, y_train, nb_epoch = 1000, batch_size = 6)
-    #保存模型权重
-    model.save_weights(modelfile)
+    model.fit(x_train, y_train, epochs = 100)
+    # #保存模型权重
+    # model.save_weights(modelfile)
     #返回训练模型
     return model
--- BP_Adaboost/Data.py
+++ BP_Adaboost/Data.py
@@ -1,1 +1,18 @@
-import pandas as pd
+import scipy.io as scio
+import numpy as np
+
+data_path="data/data.mat"
+data = scio.loadmat(data_path)
+
+input_train = np.array(data['input_train']).T
+output_train = np.array(data['output_train']).T
+input_test = np.array(data['input_test']).T
+output_test = np.array(data['output_test']).T
+
+labels = ['成分费用利润率', '资产营运能力', '公司总资产', '总资产增长率',
+          '流动比率', '营业现金流量', '审计意见类型', '每股收益',
+          '每股收益', '资产负债率']
+
+
+
+
