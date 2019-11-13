c84f30352a50bd43911c172c527b9120469a37f5
leon <applepip@163.com>
2019-11-13 16:54:16 +0800

    Evaluate Your System on the Test Set

---
+++ house_price_model/system_evaluate.py
@@ -0,0 +1,16 @@
+'''
+用测试集评估系统
+'''
+
+from model_optimizing import *
+from sklearn.metrics import mean_squared_error
+
+final_model = grid_search.best_estimator_
+X_test = strat_test_set.drop("median_house_value", axis=1)
+y_test = strat_test_set["median_house_value"].copy()
+X_test_prepared = full_pipeline.transform(X_test)
+final_predictions = final_model.predict(X_test_prepared)
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse)
+
+print(final_rmse)
